53ff6eb31d4724f2d48209af4b6e481eef02d375
kotaro-kadoshima <kadoshima.kotaro+dev@gmail.com>
2025-05-16 06:44:42 +0900

    アルゴリズムの簡素化：カードの選択ロジックを修正し、ソート後のインデックス操作を削除

--- contest/abs/abc088b/main.py
+++ contest/abs/abc088b/main.py
@@ -1,22 +1,14 @@
 n = int(input())
 cards = list(map(int, input().split()))
+cards.sort(reverse=True)
 
-flg = True
 alice = 0
 bob = 0
-for i in range(n):
-    index = 0
-    max = cards[index]
-    for j in range(len(cards)):
-        if max < cards[j]:
-            index = j
-            max = cards[j]
-    cards.pop(index)
 
-    if flg:
-        alice += max
+for i in range(n):
+    if i % 2 == 0:
+        alice += cards[i]
     else:
-        bob += max
-    flg = not flg
+        bob += cards[i]
 
 print(alice - bob)

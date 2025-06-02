525adfa214e3b2d78647031d2e20ce8717898e0a
kotaro-kadoshima <kadoshima.kotaro+dev@gmail.com>
2025-06-03 08:51:29 +0900

    二次元の累積和-A08 完了

--- contest/tessoku-book/a08/main.py
+++ contest/tessoku-book/a08/main.py
@@ -1,26 +1,19 @@
 H, W = map(int, input().split())
-X = [[0]] * (H + 1 * W + 1)
-
+X = [0] * H
+Z = [[0] * (W + 1) for _ in range(H + 1)]
 for i in range(H):
     X[i] = list(map(int, input().split()))
 
-for i in range(H):
-    sum = 0
-    for j in range(W):
-        sum += X[i][j]
-        X[i][j] = sum
 
-for j in range(W):
-    sum = 0
-    for i in range(H):
-        sum += X[i][j]
-        X[i][j] = sum
+for i in range(1, H + 1):
+    for j in range(1, W + 1):
+        Z[i][j] = Z[i][j - 1] + X[i - 1][j - 1]
 
-for i in range(H):
-    print(X[i])
+for j in range(1, W + 1):
+    for i in range(1, H + 1):
+        Z[i][j] = Z[i - 1][j] + Z[i][j]
 
 Q = int(input())
-
 for i in range(Q):
     A, B, C, D = map(int, input().split())
-    print()
+    print(Z[A - 1][B - 1] + Z[C][D] - Z[C][B - 1] - Z[A - 1][D])

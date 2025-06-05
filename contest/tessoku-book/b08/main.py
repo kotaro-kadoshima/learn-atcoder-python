e0d948c37c0e052d9fc2bae5d958fcda624f738a
kotaro-kadoshima <kadoshima.kotaro+dev@gmail.com>
2025-06-05 23:57:12 +0900

    二次元の累積和-B08 途中

--- contest/tessoku-book/b08/main.py
+++ contest/tessoku-book/b08/main.py
@@ -1,8 +1,14 @@
 N = int(input())
 X = [0] * N
 Y = [0] * N
+x_max, y_max = 0, 0
 for i in range(N):
     X[i], Y[i] = map(int, input().split())
+    if x_max < X[i]:
+        x_max = X[i]
+    if y_max < Y[i]:
+        y_max = Y[i]
+
 
 Q = int(input())
 a = [0] * Q

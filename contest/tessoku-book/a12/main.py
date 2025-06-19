42187830a69094bef6227ae8a62eaa8f9a5d051b
kotaro-kadoshima <kadoshima.kotaro+dev@gmail.com>
2025-06-19 23:34:37 +0900

    答えで二分探索-A12 完了

--- contest/tessoku-book/a12/main.py
+++ contest/tessoku-book/a12/main.py
@@ -1,2 +1,21 @@
-N, K = map(int, input())
-A = list(map(int, input()))
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+L = 1
+R = 10**9
+
+
+def check(x):
+    sum = 0
+    for i in range(N):
+        sum += x // A[i]
+    return sum >= K
+
+
+while L < R:
+    C = (L + R) // 2
+    if check(C):
+        R = C
+    else:
+        L = C + 1
+
+print(L)

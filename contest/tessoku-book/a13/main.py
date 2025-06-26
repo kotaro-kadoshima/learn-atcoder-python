f035e1e5e35aaf87feb193fdc804646b5fb46bfd
kotaro-kadoshima <kadoshima.kotaro+dev@gmail.com>
2025-06-26 20:53:08 +0900

    しゃくとり法-A13 完了

--- contest/tessoku-book/a13/main.py
+++ contest/tessoku-book/a13/main.py
@@ -2,10 +2,19 @@ N, K = map(int, input().split())
 A = list(map(int, input().split()))
 R = [0] * N
 
+for i in range(0, N - 1):
+    # i=0であればR[i]=0から、そうでなければR[i]=R[i-1]からスタートする
+    if i == 0:
+        R[i] = i
+    else:
+        R[i] = R[i - 1]
+    # 差分がA[R[i]]-A[i]がKを超えないギリギリまで、R[i]を1増やす
+    while R[i] < N - 1 and A[R[i] + 1] - A[i] <= K:
+        R[i] += 1
 
-for i in range(N):
-    r = 0
-    if 0 < i:
-        r = i - 1
-    if A[r] - A[i] <= K:
-        print(A[r])
+result = 0
+# (R[N-1]-(N-1))
+for i in range(0, N - 1):
+    result += R[i] - i
+
+print(result)
